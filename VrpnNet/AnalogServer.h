be8a0faa4395bbdb6ad22f415694e6ce02a90230
Ian Curtis <i.curtis@gmail.com>
2022-08-16 13:55:13 +0100

    Fix one linker error. Add lib project paths but sanitize them to use visual studio defaults for the VRPN project. The current source spits out pc_win32

--- VrpnNet/AnalogServer.h
+++ VrpnNet/AnalogServer.h
@@ -45,10 +45,12 @@ namespace Vrpn {
 			virtual System::Boolean get();
 		}
 
+		/*
 		property cli::array<AnalogServerChannel ^> ^Channels
 		{
 			cli::array<AnalogServerChannel ^> ^get();
 		}
+		*/
 
 		void Report();
 		void Report(ServiceClass classOfService, System::DateTime time);
